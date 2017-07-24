05dae9634b5430e5e1ba39e6548409971e6baf30
Bradley Bernard <bradbernard@me.com>
2017-07-24 00:07:36 -0700

    Initial Commit

---
+++ EjectBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EjectBar
+//
+//  Created by Bradley Bernard on 7/24/17.
+//  Copyright © 2017 Bradley Bernard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
